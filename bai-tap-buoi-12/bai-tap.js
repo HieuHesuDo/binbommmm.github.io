8f26869c4bab22cb4b7a05b2962e1f613b44825f
binbommmm <dotrunghieu259@gmail.com>
2020-10-30 22:36:07 +0700

    Đây là thư mục web frontend 10

--- bai-tap-buoi-12/bai-tap.js
+++ bai-tap-buoi-12/bai-tap.js
@@ -1,49 +1,51 @@
-document.write("1.  Cho một mảng số nguyên. Viết hàm kiểm tra nếu trong mảng có phần tử âm thì thay thế nó bằng 0.");
+document.write(
+  "1.  Cho một mảng số nguyên. Viết hàm kiểm tra nếu trong mảng có phần tử âm thì thay thế nó bằng 0."
+);
 document.writeln("<br>");
-document.writeln("Chưa làm được");
+document.writeln("Nhập vào <strong>kiemTraAm</strong> >> sau đó nhập một chuỗi số >> VD: kiemTraAm([1,2,3,4,5,-6,7,8,-9])");
 document.writeln("<br><br>");
+
+function kiemTraAm(drrs) {
+    for (let i = 0; i < drrs.length; i++) {
+      if (drrs[i] < 0) {
+        drrs[i] = 0;
+      }
+    }
+    return drrs
+  }
 
 document.write("2. Viết hàm tìm số nhỏ nhất và lớn nhất trong mảng số nguyên.");
 document.writeln("<br>");
-document.writeln("Nhập vào <strong>timMaxMin</strong> >> sau đó nhập một chuỗi số >> VD: timMaxMin([1,2,3,4,5])");
+document.writeln(
+  "Nhập vào <strong>timMaxMin</strong> >> sau đó nhập một chuỗi số >> VD: timMaxMin([1,2,3,4,5])"
+);
 document.writeln("<br><br>");
 
-function timMaxMin(arrs){
-    let Max = Math.max.apply(Math,arrs);
-    let Min = Math.min.apply(Math,arrs)
-    return ("Số lớn nhất là " + Max + " Số nhỏ nhất là " + Min)
+function timMaxMin(arrs) {
+  let Max = Math.max.apply(Math, arrs);
+  let Min = Math.min.apply(Math, arrs);
+  return "Số lớn nhất là " + Max + " Số nhỏ nhất là " + Min;
 }
 
-document.write("3. Viết hàm trả về vị trí đầu tiên của số lẻ, vị trí cuối cùng của số chẵn trong mảng bất kì.");
+document.write(
+  "3. Viết hàm trả về vị trí đầu tiên của số lẻ, vị trí cuối cùng của số chẵn trong mảng bất kì."
+);
 document.writeln("<br>");
-document.writeln("Chưa làm được");
+document.writeln(
+  "Nhập vào <strong>viTriChanLe</strong> >> sau đó nhập một chuỗi số >> VD: viTriChanLe([3,5,7,9,2,6,8])"
+);
 
-
-var brrs = [];
-function soChan(brrs){
-    let soChan = [];
-    soChan = brrs.filter((number) => (number % 2 ==0));
-    return soChan;
+function viTriChanLe(brrs) {
+  let soChan;
+  let soLe;
+  soChan = brrs.filter((number) => number % 2 == 0);
+  soLe = brrs.filter((number) => number % 2 != 0);
+  return (
+    "Vị trí số lẻ đầu tiên là " +
+    brrs.indexOf(soLe[0]) +
+    " Vị trí số chẵn cuối cùng là " +
+    brrs.indexOf(soLe.length - 1)
+  );
 }
-function soLe(brrs){
-    let soLe = [];
-    soLe = brrs.filter((number) => (number % 2 ==1));
-    return soLe;
-}
-
-function viTri(){
-    return("so le la" + soLe + "so chan la " + soChan)
-}
-
-newArray = [];
-function mang(){
-    var soNguyen = [];
-    if (newArray <0){
-        newArray = 0
-    };
-    console.log(soNguyen);
-}
-mang();
-
 
 
