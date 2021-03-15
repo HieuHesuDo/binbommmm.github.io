4ecaa45073bfcad3e51bf6a4f8f8fc1780cb9158
binbommmm <dotrunghieu259@gmail.com>
2021-03-15 14:58:53 +0700

    Đây là thư mục web frontend 10

--- Hesu Studio/js/main.js
+++ Hesu Studio/js/main.js
@@ -3,23 +3,23 @@ $(document).ready(function () {
     function () {
       $(".collection-1-thumbnail img").css("opacity", "0.7");
       $(".collection-1-thumbnail img").css("transition", "200ms");
-      $(".article-button-right .backgroud-line").css("position", "absolute");
-      $(".article-button-right .backgroud-line").css(
-        "background-color",
-        "black"
-      );
-      $(".article-button-right .backgroud-line").css("width", "85px");
-      $(".article-button-right .backgroud-line").css("right", "10");
-      $(".article-button-right .backgroud-line").css("transition", "500ms");
+      // $(".article-button-right .backgroud-line").css("position", "absolute");
+      // $(".article-button-right .backgroud-line").css(
+      //   "background-color",
+      //   "black"
+      // );
+      // $(".article-button-right .backgroud-line").css("width", "85px");
+      // $(".article-button-right .backgroud-line").css("right", "10");
+      // $(".article-button-right .backgroud-line").css("transition", "500ms");
     },
     function () {
       $(".collection-1-thumbnail img").css("opacity", "1");
-      $(".article-button-right .backgroud-line").css(
-        "background-color",
-        "#d6d6d6"
-      );
-      //   $(".article-button-right .backgroud-line").css("right","inherit");
-      $(".article-button-right .backgroud-line").css("width", "243px");
+      // $(".article-button-right .backgroud-line").css(
+      //   "background-color",
+      //   "#d6d6d6"
+      // );
+      // //   $(".article-button-right .backgroud-line").css("right","inherit");
+      // $(".article-button-right .backgroud-line").css("width", "243px");
     }
   );
 });
