49730b3b288b95c19e231afb61ec187f9f57c62f
binbommmm <dotrunghieu259@gmail.com>
2021-03-20 09:58:16 +0700

    Đây là thư mục web frontend 10

--- Hesu Studio/js/main.js
+++ Hesu Studio/js/main.js
@@ -1,25 +1,13 @@
+
+// Đây là code mở ảnh thumb của collection
 $(document).ready(function () {
   $(".collection-1-thumbnail").hover(
     function () {
       $(".collection-1-thumbnail img").css("opacity", "0.7");
       $(".collection-1-thumbnail img").css("transition", "200ms");
-      // $(".article-button-right .backgroud-line").css("position", "absolute");
-      // $(".article-button-right .backgroud-line").css(
-      //   "background-color",
-      //   "black"
-      // );
-      // $(".article-button-right .backgroud-line").css("width", "85px");
-      // $(".article-button-right .backgroud-line").css("right", "10");
-      // $(".article-button-right .backgroud-line").css("transition", "500ms");
     },
     function () {
       $(".collection-1-thumbnail img").css("opacity", "1");
-      // $(".article-button-right .backgroud-line").css(
-      //   "background-color",
-      //   "#d6d6d6"
-      // );
-      // //   $(".article-button-right .backgroud-line").css("right","inherit");
-      // $(".article-button-right .backgroud-line").css("width", "243px");
     }
   );
 });
@@ -44,10 +32,83 @@ $(document).ready(function () {
     },
     function () {
       $(".collection-1-thumbnail img").css("opacity", "1");
+    } 
+  );
+});
+
+$(document).ready(function () {
+  $(".collection-2-thumbnail").hover(
+    function () {
+      $(".collection-2-thumbnail img").css("opacity", "0.7");
+      $(".collection-2-thumbnail img").css("transition", "200ms");
+    },
+    function () {
+      $(".collection-2-thumbnail img").css("opacity", "1");
     }
   );
 });
 
+$(document).ready(function () {
+  $(".collection-2-title").hover(
+    function () {
+      $(".collection-2-thumbnail img").css("opacity", "0.7");
+      $(".collection-2-thumbnail img").css("transition", "200ms");
+    },
+    function () {
+      $(".collection-2-thumbnail img").css("opacity", "1");
+    }
+  );
+});
+
+$(document).ready(function () {
+  $(".article-button-left p").hover(
+    function () {
+      $(".collection-2-thumbnail img").css("opacity", "0.7");
+      $(".collection-2-thumbnail img").css("transition", "200ms");
+    },
+    function () {
+      $(".collection-2-thumbnail img").css("opacity", "1");
+    } 
+  );
+});
+
+$(document).ready(function () {
+  $(".collection-3-thumbnail").hover(
+    function () {
+      $(".collection-3-thumbnail img").css("opacity", "0.7");
+      $(".collection-3-thumbnail img").css("transition", "200ms");
+    },
+    function () {
+      $(".collection-3-thumbnail img").css("opacity", "1");
+    }
+  );
+});
+
+$(document).ready(function () {
+  $(".collection-3-title").hover(
+    function () {
+      $(".collection-3-thumbnail img").css("opacity", "0.7");
+      $(".collection-3-thumbnail img").css("transition", "200ms");
+    },
+    function () {
+      $(".collection-3-thumbnail img").css("opacity", "1");
+    }
+  );
+});
+
+$(document).ready(function () {
+  $(".article-button-right p").hover(
+    function () {
+      $(".collection-3-thumbnail img").css("opacity", "0.7");
+      $(".collection-3-thumbnail img").css("transition", "200ms");
+    },
+    function () {
+      $(".collection-3-thumbnail img").css("opacity", "1");
+    } 
+  );
+});
+
+// Đây là code mở submenu
 function openSubmenu() {
   document.getElementById("submenu").classList.toggle("show");
 }
@@ -83,6 +144,8 @@ window.onclick = function (event) {
 //   });
 // });
 
+
+// Đây là code mở menu
 $(".menu-wrap .toggler").on("click", function () {
   if (!$(this).data("clicked")) {
     //do your stuff here if the button is not clicked
@@ -101,34 +164,49 @@ $(".menu-wrap .toggler").on("click", function () {
 });
 
 
-$(".navbar-cart-wrap img").on("click", function () {
-  if (!$(this).data("clicked")) {
-    //do your stuff here if the button is not clicked
-    $(".navbar-cart-wrap .cart").toggle("fast");
-    $(".navbar-search-wrap .search").hide("fast");
-    $("body").css("overflow", "hidden");
-    $(this).data("clicked", true);
-  } else {
-    //do your stuff here if the button is clicked
-    $(".navbar-cart-wrap .cart").toggle("fast");
-    $("body").css("overflow", "scroll");
-    $(this).data("clicked", false);
-  }
-});
+// $(".navbar-cart-wrap img").on("click", function () {
+//   if (!$(this).data("clicked")) {
+//     //do your stuff here if the button is not clicked
+//     $(".navbar-search-wrap .search").css("display","none");
+//     $(".navbar-cart-wrap .cart").show("fast");
+//     $("body").css("overflow", "hidden");
+//     $(this).data("clicked", true);
+//   } else {
+//     //do your stuff here if the button is clicked
+//     $(".navbar-cart-wrap .cart").hide("fast");
+//     $("body").css("overflow", "scroll");
+//     $(this).data("clicked", false);
+//   }
+// });
 
-$(".navbar-search-wrap img").on("click", function () {
-  if (!$(this).data("clicked")) {
-    //do your stuff here if the button is not clicked
-    $(".navbar-search-wrap .search").toggle("fast");
-    $(".navbar-cart-wrap .cart").hide("fast");
-    $("body").css("overflow", "hidden");
-    $(this).data("clicked", true);
-  } else {
-    //do your stuff here if the button is clicked
-    $(".navbar-search-wrap .search").toggle("fast");
-    $("body").css("overflow", "scroll");
-    $(this).data("clicked", false);
-  }
-});
+// $(".navbar-search-wrap img").on("click", function () {
+//   if (!$(this).data("clicked")) {
+//     //do your stuff here if the button is not clicked
+//     $(".navbar-cart-wrap .cart").css("display","none");
+//     $(".navbar-search-wrap .search").show("fast");
+//     $("body").css("overflow", "hidden");
+//     $(this).data("clicked", true);
+//   } else {
+//     //do your stuff here if the button is clicked
+//     $(".navbar-search-wrap .search").hide("fast");
+//     $("body").css("overflow", "scroll");
+//     $(this).data("clicked", false);
+//   }
+// });
+
+
+
+// Đây là code mở find và cart
+function togglediv(id) {
+  document.querySelectorAll(".TableBody").forEach(function(div) {
+    if (div.id == id) {
+      // Toggle specified DIV
+      div.style.display = div.style.display == "none" ? "block" : "none";
+    } else {
+      // Hide other DIVs
+      div.style.display = "none";
+    }
+  });
+}
 
 
